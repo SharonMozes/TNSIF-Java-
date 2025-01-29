cf54794f812bf029d80c582269f3ca6813179ee9
SharonMozes <sharonmozes24@gmail.com>
2025-01-29 23:21:43 +0530

    Day2

---
+++ SharonTNS/src/Sharon/Day2.java
@@ -0,0 +1,30 @@
+package Sharon;
+
+public class Day2 {
+	public static void main(String[] args) {
+		byte byteVar = 127;           
+        short shortVar = 32767;        
+        int intVar = 100000;           
+        long longVar = 1000000000L;    
+        float floatVar = 3.14f;       
+        double doubleVar = 3.14159265359; 
+
+        char charVar = 'A';            
+        boolean boolVar = true;     
+        
+        // Non-primitive data types
+        String stringVar = "Hello, Sharon!"; // String object
+
+        // Displaying the values of each data type
+        System.out.println( byteVar);
+        System.out.println( shortVar);
+        System.out.println( intVar);
+        System.out.println(longVar);
+        System.out.println( floatVar);
+        System.out.println(doubleVar);
+        System.out.println(charVar);
+        System.out.println( boolVar);
+        System.out.println(stringVar);
+    
+	}
+}
